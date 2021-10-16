e60c9b033faad3ac550939643368793d15316db7
tomweaver <atomweavermail@gmail.com>
2021-10-16 16:30:42 -0400

    got single operations working

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const screen = document.querySelector('.screen-contents');
-const buttons = document.querySelector('.button-container');
+const buttons = document.querySelector('.btn-container');
+const eqlBtn = document.getElementById('equals');
+const plusBtn = document.getElementById('plus');
+const minusBtn = document.getElementById('minus');
+const timesBtn = document.getElementById('times');
+const divideBtn = document.getElementById('divide');
+
 let displayValue;
+let firstVal, secondVal, operator;
 
 function add(a, b) { 
     return a + b;
@@ -31,12 +38,51 @@ function operate(a, b, op) {
     }
 }
 
-window.addEventListener('click', function(e){
+const NumsAndClearBtns = buttons.addEventListener('click', function(e){
     if(e.target.dataset.num) {
-        screen.innerText += e.target.dataset.num;
-        displayValue = parseInt(screen.innerText);
+            screen.innerText += e.target.dataset.num;
+            displayValue = parseInt(screen.innerText);
     } else if (e.target.dataset.clr) {
         screen.innerText = '';
         displayValue = 'none';
     }
 })
+
+eqlBtn.addEventListener('click', () => {
+    if (firstVal) {
+        secondVal = displayValue;
+        screen.innerText = operate(firstVal, secondVal, operator);
+    } 
+});
+
+plusBtn.addEventListener('click', () => {
+    operator = '+'
+    firstVal = displayValue;
+    // clear the screen
+    screen.innerText = '';
+    displayValue = 'none';
+});
+
+minusBtn.addEventListener('click', () => {
+    operator = '-'
+    firstVal = displayValue;
+    // clear the screen
+    screen.innerText = '';
+    displayValue = 'none';
+});
+
+timesBtn.addEventListener('click', () => {
+    operator = '*'
+    firstVal = displayValue;
+    // clear the screen
+    screen.innerText = '';
+    displayValue = 'none';
+});
+
+divideBtn.addEventListener('click', () => {
+    operator = '/'
+    firstVal = displayValue;
+    // clear the screen
+    screen.innerText = '';
+    displayValue = 'none';
+});
