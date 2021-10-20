dbb58956495ab5f3e65da7eff36cbf88cae2bae6
tomweaver <atomweavermail@gmail.com>
2021-10-20 18:16:50 -0400

    fixed double operation clicking

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ const buttons = document.querySelector('.btn-container');
 const opContainer = document.getElementById('op-container');
 const eqlBtn = document.getElementById('equals');
 const clrBtn = document.getElementById('clear');
+const numButtons = document.querySelectorAll('[data-num]');
+
+numButtons.forEach((button) => button.addEventListener('click', () => pushNumber(button.textContent)));
+eqlBtn.addEventListener('click', evaluate);
 
 let displayVal = 0;
 let firstVal = null;
 let secondVal = null;
 let operator = null;
+let opPressed = false;
+let lastPressed = '';
 
 function add(a, b) {
     return a + b;
@@ -38,6 +44,7 @@ function operate(a, b, op) {
 function screenClear() {
     screen.innerText = null;
     displayVal = 0;
+    opPressed = false;
 }
 
 function nullVals() {
@@ -46,28 +53,28 @@ function nullVals() {
     operator = null;
 }
 
-const NumBtns = buttons.addEventListener('click', function (e) {
-    if (e.target.dataset.num) {
-        if (operator !== null) { // if an operator button has already been pressed, refresh the screen for the next value
-            screenClear();
-        }
-        screen.innerText += e.target.dataset.num;
-        displayVal = parseInt(screen.innerText);
+function pushNumber(num) {
+    if (opPressed) { // if an operator button has already been pressed, refresh the screen for the next value
+        screenClear();
     }
-});
+    screen.innerText += num;
+    displayVal = parseInt(screen.innerText);
+    lastPressed = 'num';
+};
 
-eqlBtn.addEventListener('click', () => {
+function evaluate() {
     if (firstVal) {
         secondVal = displayVal;
         screen.innerText = operate(firstVal, secondVal, operator);
         displayVal = parseInt(screen.innerText);
         nullVals();
     }
-});
+}
 
 const operators = opContainer.addEventListener('click', (e) => {
-    if (displayVal === null) { 
-        return; // if no numbers have been entered, ignore;
+    if (displayVal === null)  return; // if no numbers have been entered, ignore;
+    if (lastPressed === 'op') {
+        operator = e.target.dataset.op;
     } else if (operator !== null && firstVal !== null) {
         screen.innerText = operate(firstVal, displayVal, operator);
         displayVal = parseInt(screen.innerText);
@@ -78,9 +85,13 @@ const operators = opContainer.addEventListener('click', (e) => {
         firstVal = displayVal;
         operator = e.target.dataset.op;
     }
+    opPressed = true;
+    lastPressed = 'op';
 });
 
 clrBtn.addEventListener('click', () => {
     screenClear();
     nullVals();
 });
+
+
